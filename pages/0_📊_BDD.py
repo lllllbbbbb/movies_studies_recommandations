5c4ae16f430d9f240ef7126e801f0b1450300172
lllllbbbbb <lo.baret@gmail.com>
2023-12-05 12:15:38 +0100

    Create 0_📊_BDD.py

---
+++ pages/0_📊_BDD.py
@@ -0,0 +1,4 @@
+import streamlit as st
+#st.title("Etat de nos tables de la base de données IMDB")
+st.markdown("<h1 style='text-align: center; color: white;'>Etat du renseignement des tables de la Base De Données IMDB</h1>", unsafe_allow_html=True)
+st.image("first_graph.png")
