c734131f3562f8030bfeb46ac90edb1a89a4f551
lllllbbbbb <lo.baret@gmail.com>
2023-12-05 12:13:24 +0100

    Add files via upload

---
+++ Accueil.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Dashboard Cinema",
+    page_icon="👋",
+)
+
+#st.write("# Bienvenue sur notre application pour le Projet 2! 👋")
+st.markdown(
+    """
+"""
+)
+st.markdown(
+    """
+"""
+)
+st.markdown(
+    """
+"""
+)
+st.markdown(
+    """
+"""
+)
+st.markdown(
+    """
+"""
+)
+st.markdown(
+    """
+"""
+)
+st.image('logo.jpg')
+
+#st.markdown("<h3 style='text-align: left; color: white;'>Lorraine B, Marie M, Lô B</h1>", unsafe_allow_html=True)
+st.sidebar.success("Veuillez selectionner une page")
+
+
